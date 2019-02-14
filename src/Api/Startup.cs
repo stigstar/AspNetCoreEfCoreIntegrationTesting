06c6439e2bef504271eb15316801102e98d0faec
Stig Stavik <stig.stavik@gmail.com>
2019-02-14 22:35:13 +0100

    .

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Api
